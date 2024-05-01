1567b005467eae1510e0fc63e3437a09bc734684
Tindecken <tindecken@gmail.com>
2024-05-01 16:56:46 +0700

    so far so gôd

--- controllers/files/getPaperWorks.ts
+++ controllers/files/getPaperWorks.ts
@@ -1,10 +1,10 @@
 import { Elysia, t } from 'elysia'
 import { userInfo } from '../../middlewares/userInfo'
 import { createInsertSchema } from "drizzle-typebox"
-import { files, usersFiles, paperWorks } from '../../drizzle/schema/schema'
+import { files, usersFiles, paperWorks, categories } from '../../drizzle/schema/schema'
 import db from '../../drizzle/db'
 import type { GenericResponseInterface } from '../../models/GenericResponseInterface';
-import {eq} from "drizzle-orm";
+import {eq, getTableColumns, asc, desc} from "drizzle-orm";
 
 
 export const getPaperWorks = (app: Elysia) =>
@@ -20,18 +20,28 @@ export const getPaperWorks = (app: Elysia) =>
         if (userInfo.fileId == undefined) {
             throw new Error('No file is selected')
         }
-        const filePaperWorks = await db.query.files.findFirst({
-            offset: 1,
-            where: eq(files.id, 1),
-            with: {
-                categories: {
-                    with: {
-                        paperWorks: {
-                            limit: pageSize,
-                    }
-                }
-            }
-        }})
+        // const filePaperWorks = await db.query.files.findFirst({
+        //     where: eq(files.id, 1),
+        //     with: {
+        //         categories: {
+        //             with: {
+        //                 paperWorks: {
+        //                     limit: 1000,
+        //             }
+        //         }
+        //     }
+        // }})
+        // return filePaperWorks
+        // const allPPWs = await db.select().from(paperWorks)
+        const filePaperWorks = await db.select({
+            paperWork: getTableColumns(paperWorks)
+        }).from(paperWorks)
+            .leftJoin(categories, eq(paperWorks.categoryId, categories.id))
+            .leftJoin(files, eq(files.id, categories.fileId))
+            .where(eq(files.id, 1))
+            .orderBy(desc(orderBy ? paperWorks['orderBy'] : paperWorks.createdAt))
+            .limit(1)
+            .offset(1)
         return filePaperWorks
     }, {
         query: t.Object({
