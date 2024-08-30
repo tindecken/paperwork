5c8d5bf9a0f370ad3b60adafffe6651d0fcc40cf
Tindecken <tindecken@gmail.com>
2024-08-30 22:01:35 +0700

    Stable - ongoing getPaperworks

--- controllers/paperworks/index.ts
+++ controllers/paperworks/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from 'elysia'
 import { createPaperWork } from './create'
 import { updatePaperWork } from './update'
 import { deletePaperWork } from './delete'
+import { getByFileid } from './getByFileId'
 
 // Notes: deleteFile not imported for safety reasons
 
@@ -9,3 +10,4 @@ export const paperworksController = new Elysia({ prefix: '/paperworks'})
   .use(createPaperWork)
   .use(updatePaperWork)
   .use(deletePaperWork)
+  .use(getByFileid)
