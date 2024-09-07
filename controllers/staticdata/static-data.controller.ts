a87510f12598326d54e44b6814d04b3f1e308659
Tindecken <tindecken@gmail.com>
2024-09-07 21:38:45 +0700

    test ci

--- controllers/staticdata/static-data.controller.ts
+++ controllers/staticdata/static-data.controller.ts
@@ -11,7 +11,7 @@ export const staticDataController = (app: Elysia) =>
             <title>Elysia HTML Page</title>
           </head>
           <body>
-            <h1>Hello World!</h1>
+            <h1>Hello World!!</h1>
           </body>
       </html>`
             )
