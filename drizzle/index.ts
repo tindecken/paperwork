3ab0b7edbecd212d580723dfe4ec2945982c88b3
Tindecken <tindecken@gmail.com>
2024-09-08 21:22:00 +0700

    Remove dotenv, using bun --env-file instead

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -1,15 +1,10 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 
-config({ path: '.env.local' }); // or .env.local
-
 const client = createClient({
-  url: 'file:mypaperwork.db',
-  // encryptionKey: Bun.env["ENCRYPTION_KEY"]
-  // url: process.env.TURSO_CONNECTION_URL!,
-  // authToken: process.env.TURSO_AUTH_TOKEN!,
+  url: Bun.env['TURSO_CONNECTION_URL']!,
+  authToken: Bun.env['TURSO_AUTH_TOKEN']!,
 });
 
 export const db = drizzle(client, { schema });
