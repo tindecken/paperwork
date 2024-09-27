a8bf022cfb77663010a1f861ba5b0321c4e937c0
Tindecken <tindecken@gmail.com>
2024-09-27 23:10:32 +0700

    so far so good

--- controllers/documents/index.ts
+++ controllers/documents/index.ts
@@ -1,9 +1,11 @@
 import { Elysia } from "elysia";
 import { addDocuments } from "./add";
 import { removeDocuments } from "./remove";
-import {setCover} from "./setCover.ts";
+import { setCover } from "./setCover.ts";
+import { download } from "./download.ts";
 
 export const documentsController = new Elysia({ prefix: "/documents" })
   .use(addDocuments)
   .use(removeDocuments)
   .use(setCover)
+  .use(download)
