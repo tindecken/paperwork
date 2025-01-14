6a4a0475f006858c64cfc8bbea71ab538c2b3f09
Tindecken <tindecken@gmail.com>
2025-01-14 22:34:05 +0700

    update drizzleconfig

--- drizzle/drizzle.config.ts
+++ drizzle/drizzle.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   out: './drizzle/migrations',
   dialect: 'turso',
   dbCredentials: {
-    url: "file:mypaperwork.db",
-    authToken: process.env['TURSO_AUTH_TOKEN']!,
-    //url: process.env['TURSO_CONNECTION_URL']!,
-    //authToken: process.env['TURSO_AUTH_TOKEN']!,
+    // url: "file:mypaperwork.db",
+    // authToken: process.env['TURSO_AUTH_TOKEN']!,
+    url: "libsql://mypaperwork-tindecken.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjU4MDE0NDIsImlkIjoiZDAyZTI2MWQtZGE3Ni00ZWVkLTg3NmEtZDY3NzY0MDNkYmU5In0.KbaMgLVtEB-IR2fW258gXniX_szkE6RgdAZaH8EbZk5C3FLmRvzHM-C7WFqwNwHExsXA69tq2kivuAnmHMllBw",
   },
 });
