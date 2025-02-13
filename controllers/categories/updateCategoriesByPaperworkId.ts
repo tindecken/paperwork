f1cb4b9d84cd541e066137b3a92cb75c97d8dbdc
Tindecken <tindecken@gmail.com>
2025-02-14 06:19:43 +0700

    refactor(categories): streamline updateCategoriesByPaperworkId logic and improve error handling for category checks

--- controllers/categories/updateCategoriesByPaperworkId.ts
+++ controllers/categories/updateCategoriesByPaperworkId.ts
@@ -1,84 +1,99 @@
-import { Elysia, t } from 'elysia';
+import { Elysia, t } from "elysia";
 import {
-    categoriesTable,
-    type InsertPaperworksCategories,
-    paperworksCategoriesTable,
-    paperworksTable
-} from '../../drizzle/schema.ts'
-import { db } from '../../drizzle/index.ts'
-import type { GenericResponseInterface } from '../../models/GenericResponseInterface.ts';
-import {eq, sql, and, ne} from "drizzle-orm"
-import {userInfo} from "../../middlewares/userInfo.ts";
-import {ulid} from "ulid";
+  categoriesTable,
+  type InsertPaperworksCategories,
+  paperworksCategoriesTable,
+  paperworksTable,
+} from "../../drizzle/schema.ts";
+import { db } from "../../drizzle/index.ts";
+import type { GenericResponseInterface } from "../../models/GenericResponseInterface.ts";
+import { eq, sql, and, ne } from "drizzle-orm";
+import { userInfo } from "../../middlewares/userInfo.ts";
+import { ulid } from "ulid";
 
 export const updateCategoriesByPaperworkId = (app: Elysia) =>
-  app
-      .use(userInfo)
-      .put('/updateCategories', async ({ userInfo, body, set }) => {
-          // check paperworkId exist or not in table paperworks
-          const existingPaperwork = await db.select().from(paperworksTable).where(eq(paperworksTable.id, body.paperworkId))
-          if (existingPaperwork.length === 0) {
-            set.status = 400
-            const res: GenericResponseInterface = {
-                success: false,
-                message: `Paperwork with id ${body.paperworkId} does not exist!`,
-                data: null
-            }
-            return res
-          }
-          // get Uncategorized category id
-					const uncategorizedCategory = await db.select().from(categoriesTable).where(
-							and(
-									eq(categoriesTable.name, 'Uncategorized'),
-									eq(categoriesTable.fileId, userInfo.selectedFileId!),
-							)
-							
-					)
-					// check Uncategorized category exist or not
-					if (uncategorizedCategory.length === 0) {
-							set.status = 400
-							const res: GenericResponseInterface = {
-									success: false,
-									message: 'Uncategorized category does not exist!',
-									data: null
-							}
-							return res
-					}
-          await db.delete(paperworksCategoriesTable).where(
-            and(
-                eq(paperworksCategoriesTable.paperworkId, body.paperworkId),
-								ne(paperworksCategoriesTable.categoryId, uncategorizedCategory[0].id)
+  app.use(userInfo).put(
+    "/updateCategories",
+    async ({ userInfo, body, set }) => {
+      // check paperworkId exist or not in table paperworks
+      const existingPaperwork = await db
+        .select()
+        .from(paperworksTable)
+        .where(eq(paperworksTable.id, body.paperworkId));
+      if (existingPaperwork.length === 0) {
+        set.status = 400;
+        const res: GenericResponseInterface = {
+          success: false,
+          message: `Paperwork with id ${body.paperworkId} does not exist!`,
+          data: null,
+        };
+        return res;
+      }
+      // get Uncategorized category id
+      const uncategorizedCategory = await db
+        .select()
+        .from(categoriesTable)
+        .where(
+          and(
+            eq(categoriesTable.name, "Uncategorized"),
+            eq(categoriesTable.fileId, userInfo.selectedFileId!)
+          )
+        );
+      // check Uncategorized category exist or not
+      if (uncategorizedCategory.length === 0) {
+        set.status = 400;
+        const res: GenericResponseInterface = {
+          success: false,
+          message: "Uncategorized category does not exist!",
+          data: null,
+        };
+        return res;
+      }
+      await db
+        .delete(paperworksCategoriesTable)
+        .where(
+          and(
+            eq(paperworksCategoriesTable.paperworkId, body.paperworkId),
+            ne(
+              paperworksCategoriesTable.categoryId,
+              uncategorizedCategory[0].id
             )
           )
-          // re-add the paperwork categories
-          await Promise.all(
-              body.categoryIds.map(async (categoryId) => {
-                  const paperworkCategory: InsertPaperworksCategories = {
-                      id: ulid(),
-                      paperworkId: body.paperworkId,
-                      categoryId,
-                      createdBy: userInfo.userName,
-                      isDeleted: 0
-                  }
-                  await db.insert(paperworksCategoriesTable).values(paperworkCategory)
-              })
-          )
-          // update paperwork updatedAt and updatedBy
-          await db.update(paperworksTable).set({
-              updatedAt: sql`(CURRENT_TIMESTAMP)`,
-              updatedBy: userInfo.userName
-          }).where(eq(paperworksTable.id, body.paperworkId))
+        );
+      // re-add the paperwork categories
+      await Promise.all(
+        body.categoryIds.map(async (categoryId) => {
+          const paperworkCategory: InsertPaperworksCategories = {
+            id: ulid(),
+            paperworkId: body.paperworkId,
+            categoryId,
+            createdBy: userInfo.userName,
+            isDeleted: 0,
+          };
+          await db.insert(paperworksCategoriesTable).values(paperworkCategory);
+        })
+      );
+      // update paperwork updatedAt and updatedBy
+      await db
+        .update(paperworksTable)
+        .set({
+          updatedAt: sql`(CURRENT_TIMESTAMP)`,
+          updatedBy: userInfo.userName,
+        })
+        .where(eq(paperworksTable.id, body.paperworkId));
 
-          // return success response with message and data null
-          const res: GenericResponseInterface = {
-              success: true,
-              message: 'Update categories successfully!',
-              data: null
-          }
-          return res
-      }, {
-          body: t.Object({
-              paperworkId: t.String(),
-              categoryIds: t.Array(t.String())
-          })
-      });
+      // return success response with message and data null
+      const res: GenericResponseInterface = {
+        success: true,
+        message: "Update categories successfully!",
+        data: null,
+      };
+      return res;
+    },
+    {
+      body: t.Object({
+        paperworkId: t.String(),
+        categoryIds: t.Array(t.String()),
+      }),
+    }
+  );
