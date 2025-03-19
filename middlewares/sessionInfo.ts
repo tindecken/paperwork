d135903989fc983ebdb9b99079931082d34faaea
hnu <hnu@sucafina.com>
2025-03-19 18:05:54 +0700

    ongoing change to better-authen

--- middlewares/sessionInfo.ts
+++ middlewares/sessionInfo.ts
@@ -3,12 +3,21 @@ import { auth } from "../libs/auth.ts";
 import { db } from "../drizzle/index.ts";
 import { eq, and, desc } from "drizzle-orm";
 import { usersFilesTable } from "../drizzle/schema.ts";
+import type { Session, User } from "better-auth/types";
 // user middleware (compute user and session and pass to routes)
+
+type AuthResult = {
+  user: User;
+  session: Session;
+  selectedFileId: string;
+  role: string;
+};
+
 const sessionInfo = new Elysia()
   .mount(auth.handler)
   .macro({
     auth: {
-      async resolve({ error, request: { headers } }) {
+      async resolve({ error, request: { headers } }): Promise<AuthResult>  {
         const session = await auth.api.getSession({
           headers,
         });
@@ -16,6 +25,7 @@ const sessionInfo = new Elysia()
         // get selectedFileId 
         let selectedFileId = ''
         let role = ''
+
         const userFiles = await db.select().from(usersFilesTable).where(
             and(
                 eq(usersFilesTable.userId, session["user"].id),
@@ -28,8 +38,8 @@ const sessionInfo = new Elysia()
             role = userFiles[0].role
         }
         return {
-          user: session["user"],
-          session: session["session"],
+          user: session["user"] as User,
+          session: session["session"] as Session,
           selectedFileId,
           role
         };
